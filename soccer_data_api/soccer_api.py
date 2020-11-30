7738edcb06e2157fef33106e803ce5b8081dd42f
Rejep Mammedov <reppon97@gmail.com>
2020-11-30 16:33:33 +0300

    top_scorer argument error fixed

--- soccer_data_api/soccer_api.py
+++ soccer_data_api/soccer_api.py
@@ -17,12 +17,12 @@ class SoccerDataAPI:
 
     def english_premier(self):
         self.get_data = GetData(CONF['leagues']['english_premier_league'])
-        response = json_response(self.get_data.get_club_name(), self.get_data.get_points(),
+        response = json_response(self.get_data.get_club_name(), self.get_data.get_position(),
+                                 self.get_data.get_points(),
                                  self.get_data.get_matches_played(), self.get_data.get_wins(),
                                  self.get_data.get_draws(), self.get_data.get_losses(),
                                  self.get_data.get_goals_for(), self.get_data.get_goals_against(),
                                  self.get_data.get_goal_diff(), self.get_data.get_top_scorer())
-
         return response
 
     def la_liga(self):
@@ -38,7 +38,8 @@ class SoccerDataAPI:
 
     def bundesliga(self):
         self.get_data = GetData(CONF['leagues']['bundesliga'])
-        response = json_response(self.get_data.get_club_name(), self.get_data.get_points(),
+        response = json_response(self.get_data.get_club_name(), self.get_data.get_position(),
+                                 self.get_data.get_points(),
                                  self.get_data.get_matches_played(), self.get_data.get_wins(),
                                  self.get_data.get_draws(), self.get_data.get_losses(),
                                  self.get_data.get_goals_for(), self.get_data.get_goals_against(),
@@ -48,7 +49,8 @@ class SoccerDataAPI:
 
     def serie_a(self):
         self.get_data = GetData(CONF['leagues']['serie_a'])
-        response = json_response(self.get_data.get_club_name(), self.get_data.get_points(),
+        response = json_response(self.get_data.get_club_name(), self.get_data.get_position(),
+                                 self.get_data.get_points(),
                                  self.get_data.get_matches_played(), self.get_data.get_wins(),
                                  self.get_data.get_draws(), self.get_data.get_losses(),
                                  self.get_data.get_goals_for(), self.get_data.get_goals_against(),
@@ -58,10 +60,15 @@ class SoccerDataAPI:
 
     def ligue_1(self):
         self.get_data = GetData(CONF['leagues']['ligue_1'])
-        response = json_response(self.get_data.get_club_name(), self.get_data.get_points(),
+        response = json_response(self.get_data.get_club_name(), self.get_data.get_position(),
+                                 self.get_data.get_points(),
                                  self.get_data.get_matches_played(), self.get_data.get_wins(),
                                  self.get_data.get_draws(), self.get_data.get_losses(),
                                  self.get_data.get_goals_for(), self.get_data.get_goals_against(),
                                  self.get_data.get_goal_diff(), self.get_data.get_top_scorer())
 
         return response
+
+
+soccer = SoccerDataAPI()
+print(soccer.english_premier())
